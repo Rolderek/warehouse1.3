ad156940ef3f43d5b6059b86ad6498bdca491fc6
restaslaci <restaslaci@gmail.com>
2023-08-14 20:22:49 +0200

    Készen van a bevételezés
    Elvileg kész van az aktuális feladatlista.
    Komissiós listától folytatni Mr.Architect-el.

---
+++ src/main/java/KommissionList.java
@@ -0,0 +1,47 @@
+public class KommissionList
+{
+
+    private InventoryContainer inventoryContainer;
+
+    public KommissionList(InventoryContainer inventoryContainer)
+    {
+        this.inventoryContainer = inventoryContainer;
+    }
+
+    public InventoryContainer getInventoryContainer()
+    {
+        return inventoryContainer;
+    }
+
+    public void setInventoryContainer(InventoryContainer newInventoryContainer)
+    {
+        inventoryContainer = newInventoryContainer;
+    }
+
+    public void getKommissionListOfOneWarehouse()
+    {
+
+    }
+
+    public void getAllKommissionListOfAllWarehouse()
+    {
+
+    }
+
+    public void refreshAKommissionListOneWarehouse()
+    {
+
+    }
+
+    public void refreshAKommissionListOfAllWarehouse()
+    {
+
+    }
+
+    public void doneKommissionList()
+    {
+        /**
+          * egy kommisiós lista alapján elküldi a tételeket
+          */
+    }
+}
